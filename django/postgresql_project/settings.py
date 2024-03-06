234d70d35caa490b21f39893f16dd7dfa0a952df
davidohara91 <davidohara91@gmail.com>
2024-03-06 17:10:44 +0000

    Adding Django & postgreSQL project

---
+++ django/postgresql_project/settings.py
@@ -0,0 +1,17 @@
+# Postgre SQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'oharadavid',
+        'PASSWORD': '6A9d9o7D@@@',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'orm',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
